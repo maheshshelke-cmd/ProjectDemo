8acae29dead9aee5d0a9c291685459f81565ccac
maheshshelke-cmd <shelke.mahendra@gmail.com>
2021-04-08 13:29:54 +0530

    first

---
+++ src/test/java/Project1/Test1.java
@@ -0,0 +1,12 @@
+package Project1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void disp1()
+	{
+		System.out.println("Hello disp11111");
+	}
+}
---
+++ src/test/java/Project1/Test2.java
@@ -0,0 +1,12 @@
+package Project1;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void disp2()
+	{
+		System.out.println("Hello disp22222");
+	}
+}
